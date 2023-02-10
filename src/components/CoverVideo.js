a54cbabd61f891b0f9d5f4344e65e0e1b6e3a289
Annie <anniehartmann97@gmail.com>
2023-02-10 16:39:41 +0300

    CoverVideo

--- src/components/CoverVideo.js
+++ src/components/CoverVideo.js
@@ -20,9 +20,69 @@ const VideoContainer = styled.section`
   }
 `;
 
+const DarkOverlay = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  z-index: 1;
+  background-color: ${(props) => `rgba(${props.theme.bodyRgba},0.6)`};
+`;
+
+const Title = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  z-index: 5;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  color: ${(props) => props.theme.text};
+  div {
+    display: flex;
+  }
+  p {
+    font-family: "Kaushan Script";
+    font-size: ${(props) => props.theme.fontBig};
+    text-shadow: 1px 1px 1px ${(props) => props.theme.body};
+  }
+  span {
+    font-size: ${(props) => props.theme.fontlg};
+    font-family: "Sirin Stencil";
+    font-weight: 500;
+    text-shadow: 1px 1px 1px ${(props) => props.theme.body};
+    margin: 0 auto;
+    text-transform: capitalize;
+  }
+`;
+
 const CoverVideo = () => {
   return (
     <VideoContainer>
+      <DarkOverlay />
+      <Title>
+        <div>
+          <p data-h1 data-scroll data-scroll-delay="0.13" data-scroll-speed="4">
+            W
+          </p>
+          <p data-scroll data-scroll-delay="0.09" data-scroll-speed="4">
+            i
+          </p>
+          <p data-scroll data-scroll-delay="0.06" data-scroll-speed="4">
+            b
+          </p>
+          <p data-scroll data-scroll-delay="0.04" data-scroll-speed="4">
+            e
+          </p>
+        </div>
+        <span data-scroll-delay="0.04" data-scroll-speed="2">
+          Inspire. Create. Belive
+        </span>
+      </Title>
       <video src={MainVideo} type="video/mp4" autoPlay muted loop></video>
     </VideoContainer>
   );
