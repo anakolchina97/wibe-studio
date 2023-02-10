a4d67565a8ddaa1349f70d46e0050fe36cca47a1
Annie <anniehartmann97@gmail.com>
2023-02-10 17:40:14 +0300

    Home

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,102 @@
+import { motion } from "framer-motion";
 import React from "react";
+import { useState } from "react";
+import styled from "styled-components";
+
+const NavContainer = styled(motion.div)`
+  position: absolute;
+  top: ${(props) => (props.click ? "0" : `-${props.theme.navHeight}`)};
+  transition: all 0.3s ease;
+  z-index: 6;
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const MenuItems = styled(motion.ul)`
+  position: relative;
+  height: ${(props) => props.theme.navHeight};
+  background-color: ${(props) => props.theme.body};
+  color: ${(props) => props.theme.text};
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  list-style: none;
+  width: 100%;
+  padding: 0 10rem;
+`;
+
+const MenuBtn = styled.button`
+  cursor: pointer;
+  background-color: ${(props) => `rgba(${props.theme.textRgba}, 1)`};
+  color: ${(props) => props.theme.body};
+  width: 15rem;
+  height: 2.5rem;
+  border: none;
+  outline: none;
+  clip-path: polygon(0 0, 100% 0, 80% 100%, 20% 100%);
+  position: absolute;
+  display: flex;
+  top: 100%;
+  left: 50%;
+  transform: translateX(-50%);
+  justify-content: center;
+  align-items: center;
+  font-size: ${(props) => props.theme.fontmd};
+  font-weight: 600;
+  text-transform: uppercase;
+`;
+
+const MenuItem = styled(motion.li)`
+  cursor: pointer;
+  text-transform: uppercase;
+  color: ${(props) => props.theme.text};
+`;
 
 const NavBar = () => {
-  return <div>NavBar</div>;
+  const [click, setClick] = useState(false);
+  return (
+    <NavContainer
+      click={+click}
+      initial={{ y: `-100%` }}
+      animate={{ y: 0 }}
+      transition={{ duration: 1, delay: 2 }}
+    >
+      <MenuItems
+        drag="y"
+        dragConstraints={{ top: 0, bottom: 70 }}
+        dragElastic={0.05}
+        dragSnapToOrigin
+      >
+        <MenuBtn onClick={() => setClick(!click)}>Menu</MenuBtn>
+        <MenuItem
+          whileHover={{ scale: 1.1, y: -5 }}
+          whileTap={{ scale: 0.9, y: 0 }}
+        >
+          Home
+        </MenuItem>
+        <MenuItem
+          whileHover={{ scale: 1.1, y: -5 }}
+          whileTap={{ scale: 0.9, y: 0 }}
+        >
+          about
+        </MenuItem>
+        <MenuItem
+          whileHover={{ scale: 1.1, y: -5 }}
+          whileTap={{ scale: 0.9, y: 0 }}
+        >
+          shop
+        </MenuItem>
+        <MenuItem
+          whileHover={{ scale: 1.1, y: -5 }}
+          whileTap={{ scale: 0.9, y: 0 }}
+        >
+          new arrival
+        </MenuItem>
+      </MenuItems>
+    </NavContainer>
+  );
 };
 
 export default NavBar;
